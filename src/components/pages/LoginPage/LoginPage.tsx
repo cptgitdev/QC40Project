2ac6006219032be32832fbe2d83b1b2db4c89aee
Sathaporn Wongkhamnoi <sathaporn.won@CPI012-SATHAWON.local>
2022-10-10 12:07:05 +0700

    first commit by sathaporn

---
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import {Box, Button, Card, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import {Formik,FormikProps} from "formik"
+import {User} from "../../../types/user.type"
+import Link from "@mui/material/Link";
+import {Link as RouterLink} from "react-router-dom"
+
+type LoginProps = {};
+
+function LoginPage(props: LoginProps) {
+  const classed: any = {
+    root: { display: "flex", justifyContent: "center", alignItems: "center",paddingTop:2 },
+    submitBtn: { marginTop: 4 },
+    cancelBtn: { marginTop: 2 },
+  };
+
+  const initialValue:User={username:"Noah x",password:""};
+
+  const showForm=({values,handleChange,handleSubmit,setFieldValue,isSubmitting}:FormikProps<User>)=>{
+    return (
+      <form noValidate onSubmit={handleSubmit}>
+        <TextField 
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          onChange={handleChange}
+          value={values.username}
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+         variant="outlined"
+         margin="normal"
+         required
+         fullWidth
+         onChange={handleChange}
+         value={values.password}
+         name="password"
+         label="Password"
+         type="password"
+         autoComplete="current-password"
+        />
+        <Button
+        sx={classed.submitBtn}
+        type="submit"
+         fullWidth
+         variant="outlined"
+         color="primary"
+         disabled={isSubmitting}
+        >
+          sign in
+        </Button>
+        <Grid container sx={{padding:2,justifyContent:"center"}}>
+          <Link component={RouterLink} to="/" variant="body2">
+            Don't have an account? Register
+          </Link>
+        </Grid>
+      </form>
+    );
+  }
+
+  return (
+    <Box sx={classed.root}>
+       <Card sx={{maxWidth:345}}>
+          <CardMedia
+            component="img"
+            alt="CPI"
+            height="200"
+            image={`${process.env.PUBLIC_URL}/images/farmqc.jpeg`}
+            title="Login Page"
+           />
+           <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Login
+            </Typography>
+            <Formik
+             initialValues={initialValue}
+             onSubmit={()=>{
+
+             }}
+            >
+              {(props)=>showForm(props)}
+            </Formik>
+           </CardContent>
+       </Card>
+    </Box>
+  );
+}
+
+export default LoginPage;
